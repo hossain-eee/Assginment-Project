594d8dbecfe7f344e3c8447c6bff649069530b59
Md. Ismail Hossain <hossain.eee042@gmail.com>
2024-07-10 13:08:49 +0600

    install google font,svg and Getx package

--- lib/application/app.dart
+++ lib/application/app.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:soft_bd/presentation/screen/home_screen.dart';
-
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 class SoftBd extends StatelessWidget {
   const SoftBd({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
+         textTheme: GoogleFonts.notoSerifTextTheme(), // Use Noto Serif Bengali
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
