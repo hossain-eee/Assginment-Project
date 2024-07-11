77d1fb57eea1d7089431e912d36305b5430ed981
Md. Ismail Hossain <hossain.eee042@gmail.com>
2024-07-11 10:46:05 +0600

    #1 apply card at UserInfo @ HomeScreen

--- lib/presentation/ui/screens/utility/app_color.dart
+++ lib/presentation/ui/screens/utility/app_color.dart
@@ -6,6 +6,7 @@ class AppColor {
   static const Color boldFont = Color(0xFF202020);
   static const Color normalFont = Color(0xFF6A6A6A); // Defined color #6A6A6A
    static const Color shadowColor = Color(0xFFCCCCCC);
+   static const Color backgroundColor = Color(0xFFFFFFFF);
 // Background color
   static const Color contentBackground = Color(0xFFF6F6F6); // Defined color #F6F6F6
 
