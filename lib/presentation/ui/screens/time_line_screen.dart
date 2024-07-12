3121fc4b8dc4064cedf8a19f8f49e3beb4aeff77
Md. Ismail Hossain <hossain.eee042@gmail.com>
2024-07-13 02:49:08 +0600

    #2 render todays date and month

--- lib/presentation/ui/screens/time_line_screen.dart
+++ lib/presentation/ui/screens/time_line_screen.dart
@@ -1,9 +1,49 @@
 import 'package:flutter/material.dart';
-class TimeLineScreen extends StatelessWidget {
-  const TimeLineScreen({super.key});
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:soft_bd/presentation/state_holder/date_controller.dart';
+import 'package:soft_bd/presentation/state_holder/time_line_controller.dart';
+import 'package:soft_bd/presentation/ui/screens/utility/app_color.dart';
+import 'package:soft_bd/presentation/ui/widgets/appbar_widget.dart';
 
+class TimeLineScreen extends StatelessWidget {
+  TimeLineScreen({super.key});
+  final TimeLineController timeLineController = Get.find<TimeLineController>();
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+  
+    return Scaffold(
+      body: SizedBox(
+        width: 375.w,
+        height: 812.h,
+        child: Column(
+          children: [
+           const AppBarWidget(text: 'সময়রেখা'),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 10.h),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'আজ, ${timeLineController.getBengaliDate()}',
+                        style: GoogleFonts.notoSerifBengali(
+                          fontSize: 16.sp,
+                          fontWeight: FontWeight.w700,
+                          color: AppColor.boldFont,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
